ca568d0152c0523138aaf9dd9ffa1ac666cb787b
Loct Strider <lkc314@qq.com>
2024-12-24 08:19:19 +0000

    fix: Add definition of strlen and strcmp

--- src/lab6/include/string.h
+++ src/lab6/include/string.h
@@ -8,4 +8,7 @@ void *memset(void *, int, uint64_t);
 // personally added
 void *memcpy(void *dst, void *src, uint64_t n);
 
+uint64_t memcmp(void *s1, void *s2, uint64_t n);
+
+uint64_t strlen(const char *s);
 #endif
